83af73e61db79036f7a89c64e3db17a3bafea34e
Vantoancodegym <vantoanftu93.com>
2021-06-08 12:53:11 +0700

    init

---
+++ addons/mobile/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mobile manage",
+    'summary': """Mobile manage""",
+    'description': """Mobile manage""",
+    'author': "vantoanftu93@gmail.com",
+    'website': "vantoanftu93@gmail.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'product',
+    ],
+    'data': [
+
+    ],
+    # 'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': True,
+}
